8911b3f6b4c3ea1b28f2a1536640a93446070fe6
Mayank87426 <154996743+Mayank87426@users.noreply.github.com>
2026-01-14 22:36:55 +0530

    Create 10-january - Minimum ASCII Delete Sum for Two Strings.cpp

---
+++ 2026/January/10-january - Minimum ASCII Delete Sum for Two Strings.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int minimumDeleteSum(string s1, string s2) {
+        int m = s1.size();
+        int n = s2.size();
+
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+
+        // Base cases
+        // If s1 is exhausted, delete all remaining chars of s2
+        for (int j = n - 1; j >= 0; j--) {
+            dp[m][j] = dp[m][j + 1] + s2[j];
+        }
+
+        // If s2 is exhausted, delete all remaining chars of s1
+        for (int i = m - 1; i >= 0; i--) {
+            dp[i][n] = dp[i + 1][n] + s1[i];
+        }
+
+        // Fill DP table
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (s1[i] == s2[j]) {
+                    dp[i][j] = dp[i + 1][j + 1];
+                } else {
+                    dp[i][j] = min(
+                        s1[i] + dp[i + 1][j],
+                        s2[j] + dp[i][j + 1]
+                    );
+                }
+            }
+        }
+
+        return dp[0][0];
+    }
+};
